2088cb26f78e226f887f098c5cc802c8da1fd4b1
SRedditeja <redditejasurappagari2004@gmail.com>
2025-07-13 16:04:45 +0530

    Add files via upload

---
+++ DJANGO/ecommerce/catalog/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+##from . import views
+##
+##urlpatterns = [
+##    path('', views.product_list, name='product_list'),
+##    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+##]
+
+
+
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='catalog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='product_list'), name='logout'),
+]
+
+
+urlpatterns += [
+    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
+    path('view_cart/', views.view_cart, name='view_cart'),
+]
+
+
+urlpatterns += [
+    path('place-order/', views.place_order, name='place_order'),
+    path('order-history/', views.order_history, name='order_history'),
+]
